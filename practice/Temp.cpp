6ad926a2a019edf4a58f30c5913a7d979b3f0fff
MD PEAR E HABIB <habib.austcse31@gmail.com>
2021-03-03 17:09:48 +0600

    second push

---
+++ practice/Temp.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <map>
+
+using namespace std;
+void Solve(vector<int> nums,int n);
+
+int main(){
+    
+ // added the two lines below for faster input
+ ios_base::sync_with_stdio(false); 
+ cin.tie(NULL);
+    
+ //taking total testcases
+ int t;
+ cin>>t;
+  while(t--){
+  
+    int n, k;
+    cin>>n;
+    //cin>>n>>k;
+
+    vector<int> nums(n);
+    //vector<string> nums(n);
+
+    for(int i=0;i<n;i++){
+      cin>>nums[i];
+    }
+
+    Solve(nums,n);
+
+  }  
+	return 0;
+}
+
+void Solve(vector<int> nums,int n){
+
+  int n1=0,n2=0,a=0,b=0;
+
+  for(int i=0;i<n;i++){
+
+  }
+
+  cout<<"5"<<endl;
+  return ;
+}
